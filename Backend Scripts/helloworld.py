a00f4ae78630102dcdf1fca386932e5ae56ed9cf
Raymond Juliano <juliano.raimundu@gmail.com>
2025-03-26 00:39:33 -0300

    hello wolrd

---
+++ Backend Scripts/helloworld.py
@@ -0,0 +1,9 @@
+print('hello world'\n)
+
+print('1. Cadastrar Restaurante')
+print('2. Lista Restaurante')
+print('3. Ativar Restaurante ')
+print('4. Sair')
+
+opcao_escolhida = input('escolha uma opção')
+print('voce escolheu a opção:', opcao_escolhida)
